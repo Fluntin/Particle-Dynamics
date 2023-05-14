2fd6fcfd036dc254e6099e70456a459e0eb1a99f
Fluntin <112729558+Fluntin@users.noreply.github.com>
2023-05-14 23:40:58 +0200

    Update dynamic_response.m

--- MATLAB code/dynamic_response.m
+++ MATLAB code/dynamic_response.m
@@ -15,6 +15,7 @@ function dynamic_response()
     % Calculate zeta
     zeta=c/((M+m)*2*omega_n);
 
+
     % Calculate the amplitude A and phase angle alpha
     A = ((alpha * m * g)/(m+M)) / sqrt((g/delta_0 - omega^2)^2 + (c*omega/(m+M))^2);
     %alpha = atan2(2 * zeta * omega_n * omega * A, omega_n^2 - omega^2);
@@ -29,13 +30,36 @@ function dynamic_response()
     y = K + A * sin(omega * t);
 
     % Plot the dynamic response
-    plot(t, y)
-    xlabel('Time (s)')
-    ylabel('Displacement (m)')
-    title('Dynamic Response of the Structure')
+    subplot(2, 1, 1);
+    plot(t, y);
+    xlabel('Time (s)');
+    ylabel('Displacement (m)');
+    title('Dynamic Response of the Structure');
     hold on
-    plot(t, K * ones(size(t)), 'r--')
-    legend('Dynamic Response', 'Static Response')
+    plot(t, K * ones(size(t)), 'r--');
+    legend('Dynamic Response', 'Static Response');
     hold off
+
+    % Delta range for plotting
+    delta_set = linspace(0.005, 1.5, 1000);
+
+    % New omega_n
+    omega_new = sqrt(g ./delta_set);
+
+    % New Amplitude
+    A_delta = ((alpha * m * g) ./ (m + M)) ./ sqrt((g ./ delta_set - omega^2).^2 + (c * omega ./ (m + M)).^2);
+
+    % Calculate A_delta/delta_set
+    A_delta_ratio = A_delta ./ delta_set;
+
+    % Plot A_delta/delta_set vs. omega/omega_new
+    subplot(2, 1, 2);
+    plot(omega ./ omega_new, A_delta_ratio);
+    xlabel('\omega / \omega_{new}');
+    ylabel('A_{\delta} / \delta_{set}');
+    title('Amplitude Ratio vs. Frequency Ratio');
+
+
+
 end
 
