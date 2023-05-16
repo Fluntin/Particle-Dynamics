d8f1c15b04fc734935af6b964e948e8a317b2d97
Fluntin <112729558+Fluntin@users.noreply.github.com>
2023-05-16 08:36:27 +0200

    Update contiur_plot_new.m

--- MATLAB code/contiur_plot_new.m
+++ MATLAB code/contiur_plot_new.m
@@ -7,19 +7,21 @@ function contour_plot()
     g = 9.81; % m/s^2
     k = 210915; %N/m => Got it from the original by using k=((m+M)*g)/delta_0
 
-    f = linspace(1, 5, 1000); % Hz, updated range from 1 to 5
-    m = linspace(0, 750, 1000); % kg, updated range from 75 to 750
+    f = linspace(1, 5, 1000); % Hz, range from 1 to 5
+    m = linspace(0, 750, 1000); % kg, range from 75 to 750
     delta=((m+M).*g)./k;
 
 
     % Calculate omega and omega_n
     [Frekvens, masa] = meshgrid(f, m);
-    omega = 2 * pi * Frekvens; % Calculate omega using X instead of f
+    
+    omega = 2 * pi * Frekvens;
 
     Amplitude = ((alpha * masa * g) ./ (masa + M)) ./ sqrt((g ./ delta - omega.^2).^2 + ((c * omega) ./ (masa + M)).^2);
     Displacment = delta + Amplitude;
 
     surf(Frekvens, masa, Displacment);
     contourf(Frekvens, masa, Displacment);
+    colorbar;
 
 end
